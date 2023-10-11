91d077b8887f4bfd21dd8a31ffefdca251f61643
Ali Jawadi <ali.j@selected.org>
2023-10-11 18:33:43 +0330

    Add migrations for playlists.

---
+++ database/migrations/2023_10_11_134834_create_media_playlist_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('media_playlist', function (Blueprint $table) {
+            $table->foreignId('media_id')->constrained('media')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('playlist_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->unsignedInteger('order_column')->nullable()->index();
+            $table->timestamps();
+            $table->audits();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('media_playlist');
+    }
+};
