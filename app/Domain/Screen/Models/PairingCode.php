b43e709735ffb5dfd525d0feea55b81498a6f6fd
Hamed Azimi <hamed@selected.org>
2023-10-23 15:28:43 +0330

    PairingCode model changed

--- app/Domain/Screen/Models/PairingCode.php
+++ app/Domain/Screen/Models/PairingCode.php
@@ -9,6 +9,32 @@ class PairingCode extends Model
 {
     use HasFactory;
 
+    protected $table = "pairing_codes";
+
+    protected $fillable = [
+        "screen_id",
+        "organization_id",
+        "code",
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     * integer, real, float, double, string, boolean, object, array,
+     * collection, date, datetime, and timestamp
+     *
+     * @var array
+     */
+    protected $casts = [
+        "screen_id" => "integer",
+        "organization_id" => "integer",
+        "code" => "string",
+    ];
+
+    protected $dates = [
+        "created_at",
+        "updated_at",
+    ];
+
     protected $guarded = [
         'id'
     ];
