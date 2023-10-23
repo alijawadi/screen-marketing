008240671c577230ef2b886b16184604be487949
Hamed Azimi <hamed@selected.org>
2023-10-23 15:25:16 +0330

    PlaylistItem model changed

--- app/Domain/Media/Models/PlaylistItem.php
+++ app/Domain/Media/Models/PlaylistItem.php
@@ -9,7 +9,43 @@ class PlaylistItem extends Model
 {
     use HasFactory;
 
+    protected $table = "playlists";
+
+    protected $fillable = [
+        "created_by",
+        "updated_by",
+        "item_id",
+        "playlist_id",
+        "item_type",
+        "duration",
+        "order_column",
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     * integer, real, float, double, string, boolean, object, array,
+     * collection, date, datetime, and timestamp
+     *
+     * @var array
+     */
+    protected $casts = [
+        "created_by" => "integer",
+        "updated_by" => "integer",
+        "item_id" => "integer",
+        "playlist_id" => "integer",
+        "item_type" => "string",
+        "duration" => "integer",
+        "order_column" => "integer",
+    ];
+
+    protected $dates = [
+        "created_at",
+        "updated_at",
+    ];
+
     protected $guarded = [
         "id"
     ];
+
+
 }
