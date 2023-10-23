b89b98d1fea9ef734f1e3638d4dafe8d1e18a502
Hamed Azimi <hamed@selected.org>
2023-10-23 15:21:18 +0330

    Media model changed

--- app/Domain/Media/Models/Media.php
+++ app/Domain/Media/Models/Media.php
@@ -12,6 +12,58 @@ class Media extends Model implements HasMedia
 {
     use InteractsWithMedia;
 
+    protected $table = "media";
+
+    protected $fillable = [
+        "created_by",
+        "updated_by",
+        "uuid",
+        "collection_name",
+        "name",
+        "file_name",
+        "mime_type",
+        "disk",
+        "conversions_disk",
+        "size",
+        "manipulations",
+        "custom_properties",
+        "generated_conversions",
+        "responsive_images",
+        "order_column",
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     * integer, real, float, double, string, boolean, object, array,
+     * collection, date, datetime, and timestamp
+     *
+     * @var array
+     */
+    protected $casts = [
+        "created_by" => "integer",
+        "updated_by" => "integer",
+        "uuid" => "string",
+        "collection_name" => "string",
+        "name" => "string",
+        "file_name" => "string",
+        "mime_type" => "string",
+        "disk" => "string",
+        "conversions_disk" => "string",
+        "size" => "integer",
+        "manipulations" => "object",
+        "custom_properties" => "object",
+        "generated_conversions" => "object",
+        "responsive_images" => "object",
+        "order_column" => "integer",
+    ];
+
+    protected $dates = [
+        "created_at",
+        "updated_at",
+        "deleted_at",
+    ];
+
+
     public function registerMediaConversions(SpatieMedia $media = null): void
     {
         $this
