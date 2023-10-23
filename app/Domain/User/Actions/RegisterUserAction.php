878fb395233d2c4d07587321638ab869e59de9f9
Hamed Azimi <hamed@selected.org>
2023-10-24 00:46:49 +0330

    create user changed

--- app/Domain/User/Actions/RegisterUserAction.php
+++ app/Domain/User/Actions/RegisterUserAction.php
@@ -2,8 +2,10 @@
 
 namespace Domain\User\Actions;
 
+use App\Domain\Media\Models\Folder;
 use App\Domain\User\DataTransferObjects\AuthUserDTO;
 use App\Domain\User\DataTransferObjects\UserDTO;
+use Domain\User\Models\Organization;
 use Domain\User\Models\User;
 use Lorisleiva\Actions\Concerns\AsAction;
 
@@ -16,11 +18,32 @@ class RegisterUserAction
      */
     public function handle(UserDTO $data): AuthUserDTO
     {
-        $user = new User();
-        $user->name = $data->name;
-        $user->email = $data->email;
-        $user->password = $data->password;
-        $user->save();
+        /** @var Organization $organization */
+        $organization = Organization::query()
+            ->create([
+                "name" => "Organization",
+                "description" => null,
+            ]);
+
+        /** @var Folder $folder */
+        $folder = Folder::query()
+            ->create([
+                "organization_id" => $organization->id,
+                "uuid" => null,
+                "name" => "root",
+            ]);
+
+        /** @var User $user */
+        $user = User::query()
+            ->create([
+                "organization_id" => $organization->id,
+                "uuid" => null,
+                "name" => $data->name,
+                "email" => $data->email,
+                "password" => $data->password,
+                "email_verified_at" => null,
+                "is_organization_owner" => true,
+            ]);
 
         return AuthUserDTO::from($user->createToken('register')->plainTextToken);
     }
