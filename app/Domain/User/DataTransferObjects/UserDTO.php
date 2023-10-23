16e1970f81f63367db02da32105b3d91e4d76b18
Hamed Azimi <hamed@selected.org>
2023-10-24 00:59:05 +0330

    UserDTO changed

--- app/Domain/User/DataTransferObjects/UserDTO.php
+++ app/Domain/User/DataTransferObjects/UserDTO.php
@@ -8,7 +8,7 @@ use Spatie\LaravelData\Optional;
 class UserDTO extends Data
 {
     public function __construct(
-        public string          $name,
+        public string|Optional $name,
         public string          $email,
         public string|Optional $password,
         public int|Optional    $organization_id
