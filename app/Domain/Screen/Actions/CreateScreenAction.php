c0eabb209f8035a5394b3dc85b6c90ab9090f8bf
Hamed Azimi <hamed@selected.org>
2023-10-24 01:46:03 +0330

    query changed in CreateScreenAction

--- app/Domain/Screen/Actions/CreateScreenAction.php
+++ app/Domain/Screen/Actions/CreateScreenAction.php
@@ -5,14 +5,15 @@ declare(strict_types=1);
 namespace Domain\Screen\Actions;
 
 use Domain\Screen\Models\Screen;
+use Illuminate\Database\Eloquent\Model;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 class CreateScreenAction
 {
     use AsAction;
 
-    public function handle(array $data = []): Screen
+    public function handle(array $data): Screen|Model
     {
-        return Screen::create($data);
+        return Screen::query()->create($data);
     }
 }
