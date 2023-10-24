d744a6f4aaf41c18f195efaf817b634ac23208bd
Hamed Azimi <hamed@selected.org>
2023-10-24 17:28:14 +0330

    relations changed in 'Organization'

--- app/Domain/User/Models/Organization.php
+++ app/Domain/User/Models/Organization.php
@@ -7,7 +7,6 @@ use App\Domain\Media\Models\Template;
 use Domain\Screen\Models\Screen;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use OwenIt\Auditing\Contracts\Auditable;
 use \OwenIt\Auditing\Auditable as AuditableTrait;
@@ -47,26 +46,22 @@ class Organization extends Model implements Auditable
 
     public function users(): HasMany
     {
-        return $this->hasMany(User::class);
+        return $this->hasMany(User::class, "organization_id", "id");
     }
 
     public function folders(): HasMany
     {
-        return $this->hasMany(Folder::class);
-    }
-
-    public function rootFolder(): BelongsTo
-    {
-        return $this->belongsTo(Folder::class, 'root_folder_id');
-    }
-
-    public function screens(): HasMany
-    {
-        return $this->hasMany(Screen::class);
+        return $this->hasMany(Folder::class, "organization_id", "id");
     }
 
     public function templates(): HasMany
     {
-        return $this->hasMany(Template::class);
+        return $this->hasMany(Template::class, "organization_id", "id");
     }
+
+    public function screens(): HasMany
+    {
+        return $this->hasMany(Screen::class, "organization_id", "id");
+    }
+
 }
