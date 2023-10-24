a4555d41849c60c37df5b08e91a06336f19a0ebc
Hamed Azimi <hamed@selected.org>
2023-10-24 19:39:52 +0330

    clean code

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,10 +38,10 @@ class Kernel extends HttpKernel
 //            \Illuminate\Routing\Middleware\SubstituteBindings::class,
 //        ],
 
-        'api' => [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
+//        'api' => [
+//            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+//            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+//        ],
     ];
 
     /**
