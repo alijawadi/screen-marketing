cd98ab1486964cf943d0709ffc954adbc057d8b9
Hamed Azimi <hamed@selected.org>
2023-10-24 17:30:15 +0330

    relations changed in 'Folder'

--- app/Domain/Media/Models/Folder.php
+++ app/Domain/Media/Models/Folder.php
@@ -2,8 +2,10 @@
 
 namespace App\Domain\Media\Models;
 
+use Domain\User\Models\Organization;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Spatie\Image\Manipulations;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
@@ -52,4 +54,10 @@ class Folder extends Model implements HasMedia
             ->fit(Manipulations::FIT_CROP, 300, 300)
             ->nonQueued();
     }
+
+    public function organization(): BelongsTo
+    {
+        return $this->belongsTo(Organization::class, "organization_id");
+    }
+
 }
