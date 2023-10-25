e363754eaf3d4e6286a3bd740aadcf82427d327a
Hamed Azimi <hamed@selected.org>
2023-10-25 20:06:40 +0330

    add 'uploadFile' to 'AwsService'

--- app/Services/AwsService.php
+++ app/Services/AwsService.php
@@ -11,15 +11,38 @@ class AwsService
     public function createDirectory(string $directoryName): string|null
     {
         /** @var S3Client $s3 */
-        $s3 = App::make('aws')->createClient('s3');
+        $s3 = App::make("aws")->createClient("s3");
 
         /** @var Result $result */
         $result = $s3->putObject([
-            'Bucket' => env('AWS_BUCKET'),
-            'Key' => env("AWS_ENV") . "/" . $directoryName . "/1",
+            "Bucket" => env("AWS_BUCKET"),
+            "Key" => env("AWS_ENV") . "/" . $directoryName . "/1",
         ]);
 
         return $result->get("ObjectURL");
     }
+
+    public function uploadFile(string $directoryName, $file): string|null
+    {
+        /** @var S3Client $s3 */
+        $s3 = App::make("aws")->createClient("s3");
+
+        try {
+            /** @var Result $result */
+            $result = $s3->putObject([
+                "Bucket" => env("AWS_BUCKET"),
+                "Key" => env("AWS_ENV") . "/" . $directoryName,
+                "SourceFile" => $file,
+            ]);
+
+            return $result->get("ObjectURL");
+
+        } catch (\Exception $exception) {
+
+        }
+
+        return null;
+    }
+
 
 }
