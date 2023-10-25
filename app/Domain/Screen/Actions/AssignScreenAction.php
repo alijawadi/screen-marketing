300a96dafaf090a13526d1c7a1daee6e2d7b779b
Hamed Azimi <hamed@selected.org>
2023-10-25 14:59:49 +0330

    update screen query changed

--- app/Domain/Screen/Actions/AssignScreenAction.php
+++ app/Domain/Screen/Actions/AssignScreenAction.php
@@ -5,6 +5,7 @@ namespace Domain\Screen\Actions;
 use App\Domain\Screen\Events\ScreenAddedToOrganizationEvent;
 use Domain\Screen\Models\PairingCode;
 use Domain\Screen\Models\Screen;
+use Domain\User\Models\Organization;
 use Illuminate\Support\Facades\Broadcast;
 use Lorisleiva\Actions\Concerns\AsAction;
 
@@ -20,9 +21,21 @@ class AssignScreenAction
         /** @var Screen $screen */
         $screen = Screen::query()->find($pairingCode->screen_id);
 
+        /** @var Organization $organization */
+        $organization = Organization::query()
+            ->where("id", "=", $data["organization_id"])
+            ->select(["id", "country", "city", "street", "postcode", "lat", "lon"])
+            ->first();
+
         $screen->update([
             "organization_id" => $data["organization_id"],
             "created_by" => $data["created_by"],
+            "country" => $organization->country,
+            "city" => $organization->city,
+            "street" => $organization->street,
+            "postcode" => $organization->postcode,
+            "lat" => $organization->lat,
+            "lon" => $organization->lon,
         ]);
 
         $pairingCode->update([
