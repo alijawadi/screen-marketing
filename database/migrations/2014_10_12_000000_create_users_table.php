dd9977f34689a790441db455d469cc03e3a3c9b7
Hamed Azimi <hamed@selected.org>
2023-10-25 12:41:26 +0330

    field changed in 'User'

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->foreignId('parent_id')->nullable()->constrained("users", "id")->cascadeOnDelete();
-            $table->foreignId('organization_id')->nullable()->constrained("organizations", "id")->nullOnDelete();
+            $table->foreignId('organization_id')->nullable();
             $table->uuid()->unique();
             $table->string('name');
             $table->string('email')->unique();
