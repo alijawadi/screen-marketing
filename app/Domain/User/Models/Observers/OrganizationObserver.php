c8fa8c90493263a36c8848769f41284c7f634971
Hamed Azimi <hamed@selected.org>
2023-10-30 12:23:56 +0330

    OrganizationObserver created

---
+++ app/Domain/User/Models/Observers/OrganizationObserver.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Domain\User\Models\Observers;
+
+use App\Domain\Media\Models\Folder;
+use Domain\User\Models\Organization;
+use Domain\User\Models\User;
+
+class OrganizationObserver
+{
+//    public function created(Organization $organization): void
+//    {
+//        if (!$organization->root_folder_id) {
+//            $organization->update([
+//                "root_folder_id" => Folder::create(['name' => "root"])->id
+//            ]);
+//        }
+//    }
+}
